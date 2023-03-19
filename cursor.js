63623a69ab0368338be489b53ced406702f01216
max <maxdrake46@gmail.com>
2023-03-19 22:31:51 +0000

    added fun cursor

---
+++ cursor.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.createElement("div");
+  cursor.classList.add("custom-cursor");
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
+});
